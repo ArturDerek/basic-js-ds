37bca885df01b93880e841ce876d0866646b338b
Артур Деревцов <95027038+ArturDerek@users.noreply.github.com>
2022-05-02 23:16:00 +0300

    fix

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
+const { ListNode } = require('../extensions/list-node');
 // const { ListNode } = require('../extensions/list-node.js');
 
 /**
